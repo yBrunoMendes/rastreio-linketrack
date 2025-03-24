d7e8f1a4b605794000263ab3b9c870ccf2336fb2
Bruno Mendes <balhiago13@gmail.com>
2025-03-24 16:12:42 -0300

    corrige URL da API para linketrack.com

--- index.php
+++ index.php
@@ -8,22 +8,27 @@ if (!$codigo) {
     exit;
 }
 
+// Dados de teste fornecidos pela Linketrack
 $user = 'teste';
 $token = '1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f';
 
+// Usando o domínio principal em vez do subdomínio "api."
+$url = "https://linketrack.com/track/json?user={$user}&token={$token}&codigo=" . urlencode($codigo);
+
 $curl = curl_init();
 curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.linketrack.com/track/json?user={$user}&token={$token}&codigo=" . urlencode($codigo),
+    CURLOPT_URL => $url,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_TIMEOUT => 10,
 ]);
 
 $response = curl_exec($curl);
+
 if (curl_errno($curl)) {
     echo json_encode(['error' => curl_error($curl)]);
     curl_close($curl);
     exit;
 }
-curl_close($curl);
 
+curl_close($curl);
 echo $response;
